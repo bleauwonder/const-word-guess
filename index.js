f59328673e8f5b17a19ebfb87aa9ea12e634596d
bleauwonder <elyzableau.om@gmail.com>
2019-07-10 20:16:38 -0400

    adjustments to functions

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var gLtr = [];
 
 var gameWords = ["bugle", "compass", "campfire", "creek walking", "buddy system", "horseback riding", "insect repellent", "swimming test", "ultimate frisbee", "archery", "arts and crafts", "canteen", "infirmary"];
 
-var randomWord = function (gameWords) {
+var randomWordPicker = function (gameWords) {
     var randomIndex;
     randomIndex = Math.floor(Math.random() * gameWords.length);
     console.log(gameWords[randomIndex]);
@@ -26,9 +26,20 @@ var randomWord = function (gameWords) {
 //     return false;
 // }
 
-randomWord(gameWords);
+var randomWord = randomWordPicker(gameWords);
+var word = new Word(randomWord)
+word.word
 
+// console.log(Word.checkWord);
 // isCorrectGuess(randomWord, letter);
+
+/**
+ * 
+ * generate random word
+ * pass random word into word constructor
+ * 
+ * randomword.checkword(letter)
+ */
 
 inquirer
     .prompt([
@@ -38,8 +49,13 @@ inquirer
             message: 'Guess a letter!'
         }
     ]).then(function(user) {
-        Letter.checkG(user.hasBeen);
-        Word.strRep();
+        console.log(user)
+        word.checkWord(user.guess)
+
+
+        // var word = new Word()
+    
+        word.strRep();
         if(Word.checkG(user.hasBeen)){
                 console.log("Correct!!");
         }
--- word.js
+++ word.js
@@ -1,36 +1,48 @@
 var Letter = require('./letter.js');
 
 function Word(word) {
+
+
+    /**
+     * 
+     * the actual word: [string]
+     * the puzzle state: [array]
+     * strRep = puzzle state: [string]
+     */
     
-    this.getBlanks = function() {
+    this.getBlanks = function(word) {
         var ansWordArr = [];
         for (var i = 0; i < word.length; i++) {
             var currentLetter = new Letter(word[i]);
             ansWordArr.push(currentLetter);
         }
+        console.log('here'+ansWordArr)
         return ansWordArr; 
     }
+
     this.word = this.getBlanks(word);
 
     this.strRep = function() {
-        // Letter.correctG();
-        var curWord = " ";
-        for (var i = 0; i < this.word.length; i++) {
-            var curLetter = this.word[i];
-            curWord += curLetter.strRep() + " ";
-        }
-        console.log(curWord);
-    }
-    
+        var correctLetter = new Letter()
+        Letter.correctG();
+        // var curWord = " ";
+        // for (var i = 0; i < this.word.length; i++) {
+        //     var curLetter = this.word[i];
+        //     curWord += curLetter.strRep() + " ";
+        // }
+        // console.log(curWord);
+    };
+
     this.checkWord = function(letter) {
         // Letter.checkG();
         for (var i = 0; i < this.word.length; i++) {
             var curLetter = this.word[i];
-            console.log(curLetter);
+            // console.log(curLetter);
             curLetter.checkG(letter);
         }
         return this.strRep;
-    }
+    };
 };
 
 module.exports = Word;
+
